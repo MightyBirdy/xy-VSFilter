d5a74edbdc2d61cfc07e4deb900dd552a159542e
kongfl888 <kongfl888@outlook.com>
2015-10-23 05:19:37 +0800

    CL: Add"-action rebuild"

--- build_vsfilter_vs2015.sh
+++ build_vsfilter_vs2015.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sh build_vsfilter.sh -conf Release -plat Win32 -compiler VS2015
+sh build_vsfilter.sh -conf Release -plat Win32 -action rebuild -compiler VS2015
 
-sh build_vsfilter.sh -conf Release -plat x64 -compiler VS2015
+sh build_vsfilter.sh -conf Release -plat x64 -action rebuild -compiler VS2015
 
